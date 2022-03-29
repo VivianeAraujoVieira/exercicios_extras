35a40d606403579feb6e59be46d570688d839e44
ViviAraujo <vyviaraujo@hotmail.com.br>
2022-03-29 15:51:56 -0300

    tabuada_multiplicação

---
+++ 1.4_desafio.py
@@ -0,0 +1,9 @@
+"""Refazer o desafio da tabuada, 
+utilizando o laço for, com o numero que o usuario escolher"""
+
+num = int(input("Digite um número: ")) 
+for c in range(1, 11):
+    print(f"{num} * {c} = {num*c}")
+
+
+    
