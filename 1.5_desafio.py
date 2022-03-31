15369ad37a0d0a33c70ed661c99830fa1f5acec8
ViviAraujo <vyviaraujo@hotmail.com.br>
2022-03-31 05:00:21 -0300

    Somando_numeros_pares
    somando os números pares

---
+++ 1.5_desafio.py
@@ -0,0 +1,16 @@
+
+"""Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles
+que forem pares. Se o valor for impar, desconsidere-o. """
+soma=0
+cont=0
+for c in range(1, 7):
+    n = int(input(f"Digite o {c}° valor: "))
+    if n % 2 ==0:
+        soma+=n
+        cont+=1
+print(f"Você informou {cont} valores e a soma deles foi: {soma}")
+
+    
+    
+    
+    
