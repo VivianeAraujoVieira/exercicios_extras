8b5be3f516cd8325aea2f9099091c9c9e21ec171
ViviAraujo <vyviaraujo@hotmail.com.br>
2022-03-31 06:10:58 -0300

    Update 1.4_desafio.py

--- 1.4_desafio.py
+++ 1.4_desafio.py
@@ -1,11 +1,11 @@
+
 """Refazer o desafio da tabuada, 
 utilizando o laço for, com o numero que o usuario escolher"""
 
 num = int(input("Digite um número: ")) 
-for c in range(0,1 + 1):
-    print(f"\n{num} * {c}  = {num*c}")
-    c=c+1
-print(c)
+for c in range(1, 11):
+    print(f"\n{num} * {c:2}  = {num*c}")
+    
 
 
     
